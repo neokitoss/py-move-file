5b8af7302ac05cd8fbbd9451c5d0cbf4522971ff
Nikita Yavorskyi <kpokodiji13@gmail.com>
2026-06-27 00:11:11 +0300

    move_file implement

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def move_file():
-    command = "mv file.txt first_dir/second_dir/file2.txt"
+import os
+
+
+def move_file(command: str) -> None:
     parts = command.split(" ")
+    source = parts[1]
+    destination = parts[2]
+    directory = os.path.dirname(destination)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+
+    with open(source, "r") as file_in:
+        content = file_in.read()
+    with open(destination, "w") as file_out:
+        file_out.write(content)
+    os.remove(source)
